ad174f0a623dd5a88bce1f53054813cf43bf9139
Ertuğrul Keremoğlu <ertugkeremoglu@gmail.com>
2025-04-25 22:47:50 +0300

    feat(core): create `LegacyPlugin`

--- packages/core/src/bootstrap/system-plugin-binder.ts
+++ packages/core/src/bootstrap/system-plugin-binder.ts
@@ -1,6 +1,7 @@
 import type { Context } from "../context";
 import { bindPlugin } from "../extension";
 import { createFileSystemPlugin } from "../plugins/fs";
+import { createLegacyPlugin } from "../plugins/legacy";
 import { createPackagePlugin } from "../plugins/package";
 import { createProcessPlugin } from "../plugins/process";
 import { createProjectPlugin } from "../plugins/project";
@@ -15,6 +16,7 @@ export function bindDefaultSystemPlugins(
   context: Context,
 ): void
 {
+  bindPlugin(context, createLegacyPlugin());
   bindPlugin(context, createProcessPlugin());
   bindPlugin(context, createFileSystemPlugin());
   bindPlugin(context, createPackagePlugin());
