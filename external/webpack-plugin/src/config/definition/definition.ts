7f3c21a3a5612198cab987b8b23544d0c9958c6d
Ertuğrul Keremoğlu <ertugkeremoglu@gmail.com>
2025-05-02 02:16:25 +0300

    chore(webpack-plugin): add todo for accepting function as a config definition

--- external/webpack-plugin/src/config/definition/definition.ts
+++ external/webpack-plugin/src/config/definition/definition.ts
@@ -1,5 +1,6 @@
 import type { Configuration } from "webpack";
 
+// TODO(ertgl): Accept function as webpack config definition, by providing a context argument for common use cases.
 export type ConfigDefinition<
   C extends Configuration = Configuration,
 > = (
