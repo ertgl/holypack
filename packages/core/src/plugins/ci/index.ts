90b4a4c5c2fb38005e19a2664d4c4bf73b05e2db
Ertuğrul Keremoğlu <ertugkeremoglu@gmail.com>
2025-05-04 00:19:08 +0300

    chore(core/plugins/ci): add missing export-from statement

--- packages/core/src/plugins/ci/index.ts
+++ packages/core/src/plugins/ci/index.ts
@@ -2,3 +2,4 @@ export * from "./module-augmentation";
 export * from "./plugin";
 export { createContinuousIntegrationPlugin as default } from "./plugin";
 export * from "./plugin-metadata";
+export * from "./utils";
