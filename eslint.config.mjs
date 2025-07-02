71fd904c087d44bf0487591e0b615dc89e62e43e
Ertuğrul Keremoğlu <ertugkeremoglu@gmail.com>
2025-07-02 13:55:53 +0300

    chore: update usage of holypack preset in ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from "eslint/config";
 
-import createHolypackPlugin from "@holypack/eslint-plugin";
-
-const holypack = await createHolypackPlugin();
+import holypack from "@holypack/eslint-plugin/default/sync";
 
 const ESLINT_CONFIG = defineConfig([
   ...holypack.configs.recommended,
